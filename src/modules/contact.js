db6719bf01a3d6f3626e027fcad4f608eed2a2cf
zarc1411 <jaswantrathod35@gmail.com>
2020-06-24 08:54:41 +0530

    Refactored the code a bit

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,66 +1,58 @@
-const Contact = (function(){
+const Contact = (function () {
     const contentDiv = document.getElementById("content");
     const contactDiv = document.createElement("div");
-    contactDiv.setAttribute("id" , "contactInfo");
+    contactDiv.setAttribute("id", "contactInfo");
 
-    const createTimingsDiv = () =>{
+    const createHeading = (parentDiv, text) => {
+        const div = document.createElement("span");
+        div.innerHTML = text;
+        parentDiv.appendChild(div);
+    }
+
+    const createInfo = (parentDiv, text) => {
+        const div = document.createElement("span");
+        div.innerHTML = text;
+        parentDiv.appendChild(div);
+    }
+
+    const createTimingsDiv = () => {
         const timingDiv = document.createElement("div");
         contactDiv.appendChild(timingDiv);
-        timingDiv.setAttribute("id" , "timingDiv");
+        timingDiv.setAttribute("id", "timingDiv");
         timingDiv.innerHTML = '<i class="fa fa-3x fa-cutlery" aria-hidden="true"></i>';
-
-        const timingHeading = document.createElement("span");
-        timingHeading.innerHTML = 'Timings';
-        timingDiv.appendChild(timingHeading);
-
-        const timingHours = document.createElement("span");
-        timingHours.innerHTML = '9:00am - 12:00am';
-        timingDiv.appendChild(timingHours);
+        
+        createHeading(timingDiv, "Timings");
+        createInfo(timingDiv, "9:00am - 12:00am");
     }
 
     const createPhoneDiv = () => {
         const phoneDiv = document.createElement("div");
         contactDiv.appendChild(phoneDiv);
-        phoneDiv.setAttribute("id" , "phoneDiv");
+        phoneDiv.setAttribute("id", "phoneDiv");
         phoneDiv.innerHTML = '<i class="fa fa-3x fa-phone" aria-hidden="true"></i>';
 
-        const phoneHeading = document.createElement("span");
-        phoneHeading.innerHTML = 'Phone';
-        phoneDiv.appendChild(phoneHeading);
-
-        const phoneNumber = document.createElement("span");
-        phoneNumber.innerHTML = '123456567899';
-        phoneDiv.appendChild(phoneNumber);
+        createHeading(phoneDiv, "Phone");
+        createInfo(phoneDiv, "1234567899");
     }
 
     const createEmailDiv = () => {
         const emailDiv = document.createElement("div");
         contactDiv.appendChild(emailDiv);
-        emailDiv.setAttribute("id" , "emailDiv");
+        emailDiv.setAttribute("id", "emailDiv");
         emailDiv.innerHTML = '<i class="fa fa-3x fa-envelope-o" aria-hidden="true"></i>';
 
-        const emailHeading = document.createElement("span");
-        emailHeading.innerHTML = 'Email';
-        emailDiv.appendChild(emailHeading);
-
-        const emailAddress = document.createElement("span");
-        emailAddress.innerHTML = 'yourhome@gmail.com';
-        emailDiv.appendChild(emailAddress);
+        createHeading(emailDiv, "Email");
+        createInfo(emailDiv, "yourhome@gmail.com");
     }
 
     const createLocationDiv = () => {
         const locationDiv = document.createElement("div");
         contactDiv.appendChild(locationDiv);
-        locationDiv.setAttribute("id" , "locationDiv");
+        locationDiv.setAttribute("id", "locationDiv");
         locationDiv.innerHTML = '<i class="fa fa-3x fa-map-marker" aria-hidden="true"></i>';
 
-        const locationHeading = document.createElement("span");
-        locationHeading.innerHTML = 'Location';
-        locationDiv.appendChild(locationHeading);
-
-        const locationAddress = document.createElement("span");
-        locationAddress.innerHTML = 'Tokyo, Japan';
-        locationDiv.appendChild(locationAddress);
+        createHeading(locationDiv, "Location");
+        createInfo(locationDiv, "Tokyo, Japan");
     }
 
     const createContact = () => {
@@ -71,7 +63,7 @@ const Contact = (function(){
         contentDiv.appendChild(contactDiv);
     }
 
-    return {createContact};
+    return { createContact };
 })();
 
 export default Contact;
